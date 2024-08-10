b60ad055e4244b55b005b0e9da00890aa9ed6c27
Ronaldo Aparecido da Silva <ronaldoapsilva_@hotmail.com>
2024-08-10 10:46:15 +0000

    Criando testes do metodo Get

--- application/db/product_test.go
+++ application/db/product_test.go
@@ -1,8 +1,12 @@
-package db
+package db_test
 
 import (
 	"database/sql"
 	"log"
+	"testing"
+
+	"github.com/Educaction/go-hexagonal/application/db"
+	"github.com/stretchr/testify/require"
 )
 
 var Db *sql.DB
@@ -15,11 +19,11 @@ func setUp() {
 
 func createTable(db *sql.DB) {
 	table := `CREATE TABLE products (
-		"id" string,
-		"name" string,
-		"price" float,
-		"status" string
-		);`
+			"id" string,
+			"name" string,
+			"price" float,
+			"status" string
+			);`
 	stmt, err := db.Prepare(table)
 	if err != nil {
 		log.Fatal(err.Error())
@@ -28,10 +32,22 @@ func createTable(db *sql.DB) {
 }
 
 func createProduct(db *sql.DB) {
-	insert := `insert into product values("abc", "Product Test",0,"disabled")`
+	insert := `insert into products values("abc","Product Test",0,"disabled")`
 	stmt, err := db.Prepare(insert)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 	stmt.Exec()
+}
+
+func TestProductDb_Get(t *testing.T) {
+	setUp()
+	defer Db.Close()
+	productDb := db.NewProductDb(Db)
+	product, err := productDb.Get("abc")
+	require.Nil(t, err)
+	require.Equal(t, "Product Test", product.GetName())
+	require.Equal(t, 0.0, product.GetPrice())
+	require.Equal(t, "disabled", product.GetStatus())
+
 }
