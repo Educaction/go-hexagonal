28d10a9154a1484885d954833f8640572c9d74c6
Ronaldo Aparecido da Silva <ronaldoapsilva_@hotmail.com>
2024-08-22 09:44:23 +0000

    Executando webserver

--- Makefile
+++ Makefile
@@ -26,3 +26,10 @@ cobra:
 cli_create:
 	go run main.go cli -a=create -n="Product Cli" -p=25.0
 
+# o primeiro commando cria o file http.go na pasta
+# o segundo ter que ser rodado depois que atualizar o http.go 
+# o terceito em outro termina, pois de depois rodar o comando acima ele abre http://localhost:9000
+cobra_http:
+	cobra-cli add http
+	go run main.go http
+	curl http://localhost:9000/product/ff972165-3e90-4837-883f-1529ae20ae22
--- adapters/web/server/server.go
+++ adapters/web/server/server.go
@@ -12,35 +12,30 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type webServer struct {
+type Webserver struct {
 	Service application.ProductServiceInterface
 }
 
-func MakeNewWebserver() *webServer {
-	return &webServer{}
+func MakeNewWebserver() *Webserver {
+	return &Webserver{}
 }
 
-func (w webServer) server() {
-
+func (w Webserver) Serve() {
 	r := mux.NewRouter()
 	n := negroni.New(
 		negroni.NewLogger(),
 	)
-
 	handler.MakeProductHandlers(r, n, w.Service)
 	http.Handle("/", r)
 	server := &http.Server{
 		ReadHeaderTimeout: 10 * time.Second,
-		WriteTimeout:      01 * time.Second,
-		Addr:              ":8080",
-		Handler:           nil,
-		ErrorLog:          log.New(os.Stderr, "log", log.Lshortfile),
+		WriteTimeout:      10 * time.Second,
+		Addr:              ":9000",
+		Handler:           http.DefaultServeMux,
+		ErrorLog:          log.New(os.Stderr, "log: ", log.Lshortfile),
 	}
-
 	err := server.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
 }
-
-func getProduct(service application.ProductServiceInterface)
